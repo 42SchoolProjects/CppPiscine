451a7ac20446483560df880416b65798d8573375
kaveO <alban.billiette@gmail.com>
2025-08-06 17:09:06 +0200

    + | Canonical form (boring)

--- CPP-09/ex00/BitcoinExchange.hpp
+++ CPP-09/ex00/BitcoinExchange.hpp
@@ -10,8 +10,11 @@ class BitcoinExchange
 {
 	private:
 		std::map<std::string, float> Data;
+
+		BitcoinExchange();
+		BitcoinExchange(const BitcoinExchange &Other);
+		BitcoinExchange &operator=(const BitcoinExchange &Other);
 	public:
 		BitcoinExchange(const std::string &Filename);
 		void ReadAndPrintInput(const std::string &Filename) const;
 };
-
